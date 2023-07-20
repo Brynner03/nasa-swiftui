d4b4f47d5c65d6f0cd83426a60334e2824192a99
Brynner03 <bventura03@hotmail.com>
2023-07-20 13:51:59 -0400

    Initial Commit

---
+++ nasa-swiftui/nasa-swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  nasa-swiftui
+//
+//  Created by Brynner Ventura on 7/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nasa-swiftui/nasa-swiftui/nasa_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  nasa_swiftuiApp.swift
+//  nasa-swiftui
+//
+//  Created by Brynner Ventura on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct nasa_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
